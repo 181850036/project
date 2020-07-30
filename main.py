d6c463782117ea2e2317835e24f569d9c047bd9b
Strange-glow <61694837+Strange-glow@users.noreply.github.com>
2020-07-30 21:50:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from Analyser import Analyser
 from DataProcesser import DataProcesser
 from CodeProcesser import CodeProcesser
 import math
+from scipy import stats
+import numpy as np
+def verify(ultimateScore):
+    """
+    kstest方法：KS检验，参数分别是：待检验的数据，检验方法（这里设置成norm正态分布），均值与标准差
+    结果返回两个值：statistic → D值，pvalue → P值
+    p值大于0.05，为正态分布
+    H0:样本符合
+    H1:样本不符合
+    如何p>0.05接受H0 ,反之
+    """
+    u = np.mean(ultimateScore)  # 计算均值
+    std = np.std(ultimateScore)  # 计算标准差
+    print(u,std)
+    res=stats.kstest(ultimateScore, 'norm', (u, std))
+    print(res)
 def main():
     f=open('test_data.json',encoding='utf-8')
     res=f.read()
     data=json.loads(res)
-    #获得所有人做过题目的平均得分、完成题目总数、平均提交次数、总共历时和
     dataProcesser=DataProcesser(data)
     avg=dataProcesser.Avg()
     acNum=dataProcesser.AcNum()
@@ -34,11 +49,10 @@ def main():
         for j in range(0, 5):
             if (userIds[i] in ug[j]):
                 ultimateScore[i] = ultimateScore[i] / (avgPerGroup[j] / hardest)
+    '''打印中间数据'''
     for i in range(0,len(userIds)):
         print("id: "+str(userIds[i])+" 完成数:" +str(acNum[i])+" 平均分:"+str(avg[i])+" 平均提交次数:"+str(uploadNum[i])+
               " 历时:"+str(alltime[i])+" 平均debug时间:"+str(avgDebugTime[i][0])+"秒"+" 总分:"+str(ultimateScore[i]))
-    # for i in range(0,271):
-    #     print(ultimateScore[i])
     print(max(ultimateScore))
     res=[0]*10
     '''分组'''
@@ -49,8 +63,11 @@ def main():
             res[0]+=1
         else:
             res[math.floor((ultimateScore[i])) // 10-1] += 1
+    print("分数区间:",end="")
     print(res)
     ultimateScore.sort()
+    print("最终得分:",end="")
     print(ultimateScore)
+    verify(ultimateScore)
 if __name__ == '__main__':
     main()
